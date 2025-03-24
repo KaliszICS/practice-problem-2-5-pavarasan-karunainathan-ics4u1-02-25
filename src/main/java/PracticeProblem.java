3ce5a80ffcd3ad5ed76a8db1f7fac430b2042976
pavarasan-karunainathan-ics4u1-02-25 <166336911+pavarasan-karunainathan-ics4u1-02-25@users.noreply.github.com>
2025-03-24 10:15:42 -0400

    Problem Complete

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,27 +1,14 @@
 public class PracticeProblem {
+        static void insertionSort(char[] characters){
+                for(int i = 1; i < characters.length; i++){
+                        int index = i-1;
+                        char key = characters[i];
+                        for(;index >= 0 && key < characters[index]; index--){
+                                characters[index+1] = characters[index];
+                        }
+                        characters[index+1] = key;
+                }
+        }
 
-	public static void main(String args[]) {
-
-	}
-
-	public static void q1() {
-		//Write question 1 code here
-	}
-
-	public static void q2() {
-		//Write question 2 code here
-	}
-
-	public static void q3() {
-		//Write question 3 code here
-	}
-
-	public static void q4() {
-		//Write question 4 code here
-	}
-
-	public static void q5() {
-		//Write question 5 code here
-	}
-
+        public static void main(String args[]){}
 }
